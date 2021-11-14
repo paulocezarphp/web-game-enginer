36ef6551288e29f714173facb6409ef989db5d20
Cezar Leigos <cezarleigos28@outlook.com>
2021-11-14 15:57:30 -0300

    v0.0.0.2 - Adding more files
    This time we add some more files and start configuring the app.php file which will be the file responsible for managing our system operations.

--- app.php
+++ app.php
@@ -1,4 +1,35 @@
 <?php
+   
 
+   $get_operation = $_POST['app-operation'];
+
+   if($get_operation == ""){
+
+   }elseif($get_operation == ""){
+
+   }elseif($get_operation == ""){
+
+   }elseif($get_operation == ""){
+
+   }elseif($get_operation == ""){
+
+   }elseif($get_operation == ""){
+
+   }elseif($get_operation == ""){
+
+   }elseif($get_operation == ""){
+
+   }elseif($get_operation == ""){
+
+   }elseif($get_operation == ""){
+
+   }elseif($get_operation == ""){
+
+   }elseif($get_operation == ""){
+
+   }else{
+
+   }
+   
 
 ?>
