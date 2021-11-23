04f2532751d95b1cc9d64d51f14b5f12fb04e8d3
Cezar Leigos <cezarleigos28@outlook.com>
2021-11-22 22:10:09 -0300

    v0.0.0.9 - Creating classes
    This time we start to create some functions that will be responsible for manipulating and managing the database usage by the user. We also started to create some functions to manipulate and validate the admin user access to the system.

--- app/system/php/database/db-system.php
+++ app/system/php/database/db-system.php
@@ -1,5 +1,35 @@
 <?php
 
+class DbSystem{
 
+	function create_project(){
+
+	}
+
+	function save_project(){
+
+	}
+
+	function delete_project(){
+
+	}
+
+	function publish_project(){
+
+	}
+
+	function create_scene(){
+
+	}
+
+	function save_scene(){
+		
+	}
+
+	function delete_scene(){
+		
+	}
+
+}
 
 ?>
---
+++ app/system/php/database/db-user.php
@@ -0,0 +1,35 @@
+<?php
+
+class DbUser{
+
+	function login_user(){
+
+	}
+
+	function logoff_user(){
+		
+	}
+
+	function create_user(){
+
+	}
+
+	function save_user(){
+
+	}
+
+	function delete_user(){
+
+	}
+
+	function block_user(){
+
+	}
+
+	function unlock_user(){
+
+	}
+
+}
+
+?>
